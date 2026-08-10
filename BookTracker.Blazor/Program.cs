0d52d3dc432a2820466195d29e1e9353d5f28f89
punchy943 <lukasmotte75@gmail.com>
2026-08-10 11:08:07 +0200

    FEAT: blazor frontend toevoegen

---
+++ BookTracker.Blazor/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BookTracker.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"]
+    ?? throw new InvalidOperationException("ApiBaseUrl is missing.");
+
+builder.Services.AddScoped(_ =>
+    new HttpClient
+    {
+        BaseAddress = new Uri(apiBaseUrl)
+    });
+
+await builder.Build().RunAsync();
