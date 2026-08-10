49b5629cf8f262532aec3d18801aaf63208aade2
punchy943 <lukasmotte75@gmail.com>
2026-08-10 16:24:50 +0200

    FEAT: Blazor book components toevoegen

---
+++ BookTracker.Blazor/Models/Books/BookSummary.cs
@@ -0,0 +1,10 @@
+namespace BookTracker.Blazor.Models.Books;
+
+public sealed class BookSummary
+{
+    public int Id { get; set; }
+
+    public required string Title { get; set; }
+
+    public required string Author { get; set; }
+}
